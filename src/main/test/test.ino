bc26df564d7d86e4e48863096ed5b1084b798c4b
Thomas <thomas.deffradas@gmail.com>
2020-01-20 07:26:04 +0100

    Added report

--- src/main/test/test.ino
+++ src/main/test/test.ino
@@ -6,15 +6,12 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include "Arduino.h"
-// Using external librairies for the LoRa Module
-#include <LoRa_E32.h>
 
 // DS18B20 GPIO port number
 const int bus = 4;     
 const int ledPin = 5;
 const int buzzer = 2;
 
-LoRa_E32 e32(D32, D33);
 
 // Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(bus);
@@ -27,22 +24,11 @@ void setup() {
   ledcAttachPin(buzzer,0);
   Serial.begin(115200);
   sensors.begin();
-  e32.begin(866E6);
-  ResponseStatus rs = e32.sendMessage("LoRA device ON");
-  // Error Check
-  ResponseContainer rc = rs.getResponseDescription();
-  if (rc.status.code!=1){
-    rc.status.getResponseDescription();
-    exit();
-  }
 }
 
 void loop() {
   sensors.requestTemperatures(); 
-  float temperature =sensors.getTempCByIndex(0)
-  LoRa.beginPacket();
-  LoRa.print(temperature);
-  LoRa.endPacket();
+  float temperature =sensors.getTempCByIndex(0);
   if(temperature > 6 || temperature < -3){
     for (int i=0; i <= 5; i++){    
       digitalWrite (ledPin, HIGH);  // turn on the LED
@@ -52,10 +38,6 @@ void loop() {
       uint8_t octave = 1;
       ledcWriteNote(0,NOTE_C,octave); // Stop the buzzer
       delay(500); // wait for 500 milliseconds
-      LoRa.beginPacket();
-      LoRa.print("!! ALERT !!");
-      LoRa.print(temperature);
-      LoRa.endPacket();
     }
   }
   delay(500); 
